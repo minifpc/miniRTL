244dce37791e1d1e2327cb6a94b59c36187a0ca3
paule32 <paule32.jk@gmail.com>
2025-06-27 14:22:00 +0200

    update
    add little bit more parser stuff for the compiler project.

--- src/parser.pas
+++ src/parser.pas
@@ -10,7 +10,12 @@ uses
   Windows, StrUtils, Exceptions;
 
 type
-  TTokenType = (_tkIdentifier, _tkNumber, _tkAssign, _tkSemicolon, _tkEOF, _tkUnknown);
+  TTokenType = (
+    _tkKeyword, _tkIdentifier, _tkNumber,
+    _tkProgram, _tkBegin, _tkEnd, 
+    _tkAssign, _tkSemicolon, _tkDot,
+    _tkEOF, _tkUnknown
+  );
 
   TToken = record
     TokenType: TTokenType;
@@ -21,6 +26,46 @@ var
   Source: string;
   Position: Integer;
   CurrentToken: TToken;
+  Lookahead: TToken;
+
+procedure InitScanner(input: string);
+begin
+  Source := input;
+  Position := 1;
+end;
+
+function _PeekChar: Char;
+begin
+  if Position > Length(Source) then
+  result := #0 else
+  result := Source[Position];
+end;
+
+procedure SkipWhitespace;
+begin
+  while _PeekChar in [' ', #9, #10, #13] do
+  inc(Position);
+end;
+
+function ReadIdentifier: string;
+begin
+  Result := '';
+  while _PeekChar in ['A'..'Z', 'a'..'z', '0'..'9', '_'] do
+  begin
+    Result := Result + _PeekChar;
+    Inc(Position);
+  end;
+end;
+
+function ReadNumber: string;
+begin
+  Result := '';
+  while _PeekChar in ['0'..'9'] do
+  begin
+    Result := Result + _PeekChar;
+    Inc(Position);
+  end;
+end;
 
 function NextChar: Char;
 begin
@@ -34,52 +79,48 @@ procedure Advance; begin inc(Position); end;
 function GetToken: TToken;
 var
   ch: Char;
-  lex: string;
+  id: string;
 begin
-  while (NextChar in [' ', #10, #13, #9]) do
-  Advance;
+  SkipWhitespace;
+  ch := _PeekChar;
 
   ch := NextChar;
 
   if ch = #0 then
   begin
-    Result.TokenType := _tkEOF;
-    Result.Lexeme := '';
-  end else
-  if ch in ['a'..'z', 'A'..'Z'] then
-  begin
-    lex := '';
-    while NextChar in ['a'..'z', 'A'..'Z', '0'..'9', '_'] do
-    begin
-      lex := lex + NextChar;
-      Advance;
-    end;
-    CurrentToken.TokenType := _tkIdentifier;
-    CurrentToken.Lexeme    := lex;
+    CurrentToken.TokenType := _tkEOF;
+    CurrentToken.Lexeme := '';
     result := CurrentToken;
     exit;
   end else
   if ch in ['0'..'9'] then
   begin
-    lex := '';
-    while NextChar in ['0'..'9'] do
-    begin
-      lex := lex + NextChar;
-      Advance;
-    end;
     CurrentToken.TokenType := _tkNumber;
-    CurrentToken.Lexeme := lex;
+    CurrentToken.Lexeme    := ReadNumber;
     result := CurrentToken;
+    exit;
+  end else
+  if ch in ['a'..'z', 'A'..'Z'] then
+  begin
+    id := LowerCase(ReadIdentifier);
+         if id = 'program' then begin CurrentToken.TokenType := _tkProgram; result := CurrentToken; exit; end
+    else if id = 'begin'   then begin CurrentToken.TokenType := _tkBegin;   result := CurrentToken; exit; end
+    else if id = 'end'     then begin CurrentToken.TokenType := _tkEnd;     result := CurrentToken; exit; end
+    else begin
+      CurrentToken.TokenType := _tkIdentifier;
+      result := CurrentToken;
+      exit;
+    end;
+    CurrentToken.Lexeme := id;
+    Result := CurrentToken;
     exit;
   end else
   if ch = ':' then
   begin
-    lex := ':';
-    Advance;
-    if NextChar = '=' then
+    inc(Position);
+    if _PeekChar = '=' then
     begin
-      lex := lex + '=';
-      Advance;
+      Inc(Position);
       CurrentToken.TokenType := _tkAssign;
       CurrentToken.Lexeme := ':=';
       result := CurrentToken;
@@ -93,26 +134,50 @@ begin
   end else
   if ch = ';' then
   begin
-    Advance;
+    inc(Position);
     CurrentToken.TokenType := _tkSemicolon;
     CurrentToken.Lexeme := ';';
     result := CurrentToken;
+    exit;
+  end else
+  if ch = '.' then
+  begin
+    inc(Position);
+    CurrentToken.TokenType := _tkDot;
+    CurrentToken.Lexeme := '.';
   end else
   begin
+    inc(Position);
     CurrentToken.TokenType := _tkUnknown;
     CurrentToken.Lexeme := ch;
-    Advance;
     result := CurrentToken;
+    exit;
   end;
 end;
 
 procedure Match(expected: TTokenType);
 begin
-  if CurrentToken.TokenType = expected then
+  if Lookahead.TokenType = expected then
+  Lookahead := GetToken  else
+  raise Exception.Create('Syntax Error: Expected ' + IntToStr(Ord(expected)) + ' but found: ' + Lookahead.Lexeme);
+end;
+
+procedure ParseStatement;
+begin
+  if Lookahead.TokenType = _tkIdentifier then
   begin
-    CurrentToken := GetToken;
+    WriteLn('Statement: ', Lookahead.Lexeme);
+    Match(_tkIdentifier);
+    Match(_tkAssign);
+    if Lookahead.TokenType = _tkNumber then
+    begin
+      WriteLn('  Value: ', Lookahead.Lexeme);
+      Match(_tkNumber);
+    end else
+    raise Exception.Create('Syntax Error: Expected number');
+    Match(_tkSemicolon);
   end else
-  raise Exception.Create('Syntax Error: Expected ' + IntToStr(Ord(expected)) + ' but found: ' + CurrentToken.Lexeme);
+  raise Exception.Create('Syntax Error: Expected identifier');
 end;
 
 procedure ParseAssignment;
@@ -126,10 +191,12 @@ begin
       CurrentToken := GetToken;
       if CurrentToken.TokenType = _tkNumber then
       WriteLn('Found Number: ' + CurrentToken.Lexeme) else
-      raise Exception.Create('Syntax Error: Expected number');
+      raise Exception.Create('Syntax Error: Expected number.');
+      
       CurrentToken := GetToken;
       if CurrentToken.TokenType = _tkSemicolon then
-      WriteLn('Assignment OK');
+      WriteLn('Assignment OK') else
+      raise Exception.Create('Syntax Error: Expected semicolon.');
     end;
   end else
   raise Exception.Create('Syntax Error: Expected identifier');
