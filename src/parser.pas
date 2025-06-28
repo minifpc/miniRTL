fc213df9ba3b5784a98b232458264aca4c75f306
paule32 <paule32.jk@gmail.com>
2025-06-28 13:41:19 +0200

    update parser.pas
    remove "try except" block because it is not working in this state of fibo's codes

--- src/parser.pas
+++ src/parser.pas
@@ -15,6 +15,8 @@ type
     _tkProgram, _tkBegin, _tkEnd,
     _tkIf, _tkThen, _tkElse,
     _tkAssign, _tkSemicolon, _tkDot,
+    _tkPlus, _tkMinus, _tkTimes, _tkDivide,
+    _tkLParen, _tkRParen,
     _tkEOF, _tkUnknown
   );
 
@@ -27,13 +29,24 @@ var
   Source: string;
   Position: Integer;
   CurrentToken: TToken;
+var
+  right_count, left_count: Integer;
 
 procedure ParseIfStatement; forward;
+function  ParseExpression: String; forward;
 
 procedure InitScanner(input: string);
 begin
-  Source := input;
-  Position := 1;
+  Source      := input;
+  Position    := 1;
+  right_count := 0;
+  left_count  := 0;
+end;
+
+procedure yyerror(msg: String);
+begin
+  writeln('Syntax Error: ' + msg);
+  ExitProcess(2);
 end;
 
 function _PeekChar: Char;
@@ -85,7 +98,6 @@ var
 begin
   SkipWhitespace;
   ch := _PeekChar;
-
   ch := NextChar;
 
   if ch = #0 then
@@ -146,11 +158,61 @@ begin
     result := CurrentToken;
     exit;
   end else
+  if ch = '-' then
+  begin
+    inc(Position);
+    CurrentToken.TokenType := _tkMinus;
+    CurrentToken.Lexeme := '-';
+    result := CurrentToken;
+    exit;
+  end else
+  if ch = '+' then
+  begin
+    inc(Position);
+    CurrentToken.TokenType := _tkPlus;
+    CurrentToken.Lexeme := '+';
+    result := CurrentToken;
+    exit;
+  end else
+  if ch = '*' then
+  begin
+    inc(Position);
+    CurrentToken.TokenType := _tkTimes;
+    CurrentToken.Lexeme := '*';
+    result := CurrentToken;
+    exit;
+  end else
+  if ch = '/' then
+  begin
+    inc(Position);
+    CurrentToken.TokenType := _tkDivide;
+    CurrentToken.Lexeme := '/';
+    result := CurrentToken;
+    exit;
+  end else
+  if ch = '(' then
+  begin
+    inc(Position);
+    CurrentToken.TokenType := _tkLParen;
+    CurrentToken.Lexeme := '(';
+    result := CurrentToken;
+    exit;
+  end else
+  if ch = ')' then
+  begin
+    inc(Position);
+    CurrentToken.TokenType := _tkRParen;
+    CurrentToken.Lexeme := ')';
+    result := CurrentToken;
+    exit;
+  end else
   if ch = '.' then
   begin
     inc(Position);
     CurrentToken.TokenType := _tkDot;
     CurrentToken.Lexeme := '.';
+    result := CurrentToken;
+    exit;
   end else
   begin
     inc(Position);
@@ -165,7 +227,214 @@ procedure Match(expected: TTokenType);
 begin
   if CurrentToken.TokenType = expected then
   CurrentToken := GetNextToken  else
-  raise Exception.Create('Syntax Error: Expected ' + IntToStr(Ord(expected)) + ' but found: ' + CurrentToken.Lexeme);
+  yyerror('Expected ' + IntToStr(Ord(expected)) + ' but found: ' + CurrentToken.Lexeme);
+end;
+
+function ParseFactor: string;
+begin
+  case CurrentToken.TokenType of
+    _tkNumber: begin
+      result := CurrentToken.Lexeme;
+      Match(_tkNumber);
+    end;
+    _tkIdentifier: begin
+      result := CurrentToken.Lexeme;
+      Match(_tkIdentifier);
+    end;
+    _tkLParen: begin
+      Match(_tkLParen);
+      result := '(' + ParseExpression + ')';
+      Match(_tkRParen);
+    end else
+    yyerror('Unexpected token in factor: "' + CurrentToken.Lexeme + '"');
+  end;
+end;
+
+function ParseTerm: string;
+var
+  left, op, right: string;
+begin
+  left := ParseFactor;
+  while (CurrentToken.TokenType = _tkTimes)
+  or    (CurrentToken.TokenType = _tkDivide) do
+  begin
+    if CurrentToken.TokenType = _tkTimes then
+    begin
+      op := '*';
+      Match(_tkTimes);
+    end else
+    begin
+      op := '/';
+      Match(_tkDivide);
+    end;
+    right := ParseFactor;
+    left := left + ' ' + op + ' ' + right;
+  end;
+  result := left;
+end;
+
+function ParseExpression: string;
+var
+  left, op, right: string;
+begin
+  left := ParseTerm;
+  while (CurrentToken.TokenType = _tkPlus)
+  or    (CurrentToken.TokenType = _tkMinus) do
+  begin
+    if CurrentToken.TokenType = _tkPlus then
+    begin
+      op := '+';
+      Match(_tkPlus);
+    end else
+    begin
+      op := '-';
+      Match(_tkMinus);
+    end;
+    right := ParseTerm;
+    left := left + ' ' + op + ' ' + right;
+  end;
+  result := left;
+end;
+
+procedure ParseAssignment;
+var
+  varName, expr: string;
+begin
+  varName := CurrentToken.Lexeme;
+  WriteLn('Assignment an Variable: ', varName);
+  Match(_tkIdentifier);
+  Match(_tkAssign);
+  expr := ParseExpression;
+  WriteLn('  Ausdruck: ', expr);
+  Match(_tkSemicolon);
+end;
+
+procedure ParseTokenExpression;
+  procedure subParse;
+  begin
+    CurrentToken := GetNextToken;
+    if CurrentToken.TokenType = _tkLParen then
+    begin
+      writeln('TOKLEFTPAR');
+      inc(left_count);
+      CurrentToken := GetNextToken;
+      ParseTokenExpression;
+      exit;
+    end else
+    if CurrentToken.TokenType = _tkNumber then
+    begin
+      writeln('plus num: ' + CurrentToken.Lexeme);
+      CurrentToken := GetNextToken;
+      ParseTokenExpression;
+      exit;
+    end else
+    if CurrentToken.TokenType = _tkIdentifier then
+    begin
+      writeln('plus ident: ' + CurrentToken.Lexeme);
+      CurrentToken := GetNextToken;
+      ParseTokenExpression;
+      exit;
+    end else
+    yyerror('token error');
+  end;
+begin
+  if CurrentToken.TokenType = _tkPlus then
+  begin
+    writeln('plus');
+    subParse;
+  end else
+  if CurrentToken.TokenType = _tkMinus then
+  begin
+    writeln('minus');
+    subParse;
+  end else
+  if CurrentToken.TokenType = _tkTimes then
+  begin
+    writeln('times');
+    subParse;
+  end else
+  if CurrentToken.TokenType = _tkDivide then
+  begin
+    writeln('divide');
+    subParse;
+  end else
+  if CurrentToken.TokenType = _tkNumber then
+  begin
+    writeln('  valuE: ', CurrentToken.Lexeme);
+    
+    CurrentToken := GetNextToken;
+    ParseTokenExpression;
+  end else
+  if CurrentToken.TokenType = _tkRParen then
+  begin
+    inc(right_count);
+    CurrentToken := GetNextToken;
+    writeln('TOk: ' + CurrentToken.Lexeme);
+    if CurrentToken.TokenType = _tkSemicolon then
+    begin
+    writeln('lhs: ' + IntToStr(left_count));
+    writeln('rhs: ' + IntToStr(right_count));
+      if right_count < left_count  then yyerror('right paren without left paren.');
+      if left_count  < right_count then yyerror('left paren without right paren.');
+
+      right_count := 0;
+      left_count  := 0;
+      
+      writeln('semi expr end');
+      exit;
+    end else
+    if CurrentToken.TokenType = _tkRParen then
+    begin
+      inc(right_count);
+      CurrentToken := GetNextToken;
+      writeln('tok: ' + CurrentToken.Lexeme);
+      if CurrentToken.TokenType = _tkSemicolon then
+      begin
+      writeln('LHS: ' + IntToStr(left_count));
+      writeln('RHS: ' + IntToStr(right_count));
+        if right_count < left_count  then yyerror('right paren without left paren.');
+        if left_count  < right_count then yyerror('left paren without right paren.');
+        
+        right_count := 0;
+        left_count  := 0;
+        
+        writeln('end expr');
+        exit;
+      end else
+      ParseTokenExpression;
+      exit;
+    end else
+    if CurrentToken.TokenType = _tkMinus then
+    begin
+      CurrentToken := GetNextToken;
+      ParseTokenExpression;
+    end else
+    if CurrentToken.TokenType = _tkPlus then
+    begin
+      CurrentToken := GetNextToken;
+      ParseTokenExpression;
+    end else
+    if CurrentToken.TokenType = _tkTimes then
+    begin
+      CurrentToken := GetNextToken;
+      ParseTokenExpression;
+    end else
+    if CurrentToken.TokenType = _tkDivide then
+    begin
+      CurrentToken := GetNextToken;
+      ParseTokenExpression;
+    end else
+    yyerror('expression error');
+  end else
+  if CurrentToken.TokenType = _tkLParen then
+  begin
+  writeln('Lparen');
+    inc(left_count);
+    CurrentToken := GetNextToken;
+    ParseTokenExpression;
+    exit;
+  end else
+  yyerror('Expected number');
 end;
 
 procedure ParseStatement;
@@ -175,25 +444,57 @@ begin
     WriteLn('Statement: ', CurrentToken.Lexeme);
     Match(_tkIdentifier);
     Match(_tkAssign);
-    if CurrentToken.TokenType = _tkNumber then
+    while CurrentToken.TokenType <> _tkSemicolon do
     begin
-      WriteLn('  Value: ', CurrentToken.Lexeme);
-      Match(_tkNumber);
-    end else
-    raise Exception.Create('Syntax Error: Expected number');
-    Match(_tkSemicolon);
-    if CurrentToken.TokenType = _tkIdentifier then
-    ParseStatement else
-    if CurrentToken.TokenType = _tkIf then
-    writeln('IF');
-    ParseIfStatement;
+      if CurrentToken.TokenType = _tkNumber then
+      begin
+        WriteLn('  Value: ', CurrentToken.Lexeme);
+        Match(_tkNumber);
+        if CurrentToken.TokenType = _tkSemicolon then
+        begin
+          writeln('smei');
+          Match(_tkSemicolon);
+          break;
+        end else
+        if CurrentToken.TokenType = _tkPlus then
+        begin
+          CurrentToken := GetNextToken;
+          ParseTokenExpression;
+        end else
+        if CurrentToken.TokenType = _tkMinus then
+        begin
+          CurrentToken := GetNextToken;
+          ParseTokenExpression;
+        end else
+        if CurrentToken.TokenType = _tkTimes then
+        begin
+          CurrentToken := GetNextToken;
+          ParseTokenExpression;
+        end else
+        if CurrentToken.TokenType = _tkDivide then
+        begin
+          CurrentToken := GetNextToken;
+          ParseTokenExpression;
+        end;
+      
+        writeln('--> ' + CurrentToken.Lexeme);
+        if CurrentToken.TokenType = _tkSemicolon then
+        begin
+          writeln('SE');
+          break;
+        end else
+        yyerror('Semicolon expected');
+      end;
+    end;
+    //end else
+    //yyerror('Expected number');
   end else
   if CurrentToken.TokenType = _tkIf then
   begin
     writeln('if');
     ParseIfStatement;
   end else
-  raise Exception.Create('Syntax Error: Expected identifier');
+  yyerror('Expected identifier');
 end;
 
 procedure ParseIfStatement;
@@ -204,39 +505,39 @@ begin
     WriteLn('If-Bedingung: ', CurrentToken.Lexeme);
     CurrentToken := GetNextToken;
     Match(_tkThen);
-    writeln('----> ' + CurrentToken.Lexeme);
-    
+    WriteLn('If-Bedingung: ', CurrentToken.Lexeme);
     Match(_tkIdentifier);
-    writeln('----> ' + CurrentToken.Lexeme);
     
-    Match(_tkAssign);
-    writeln('----> ' + CurrentToken.Lexeme);
+    if CurrentToken.TokenType = _tkAssign then
+    begin
+        WriteLn('If-Assugn: ', CurrentToken.Lexeme);
+        Match(_tkAssign);
+    end;
     
-    CurrentToken := GetNextToken;
+    WriteLn('If-Number: ', CurrentToken.Lexeme);
+    
+    CurrentToken := GetNextToken;    
     Match(_tkElse);
-    writeln('----> ' + CurrentToken.Lexeme);
+    WriteLn('If-Else: else');
     
+    WriteLn('If-ident: ', CurrentToken.Lexeme);
     Match(_tkIdentifier);
-    writeln('---> ' + CurrentToken.Lexeme);
+    
+    WriteLn('If-Assign: ', CurrentToken.Lexeme);
     
     CurrentToken := GetNextToken;
-    Match(_tkAssign);
-    writeln('---> ' + CurrentToken.Lexeme);
+    WriteLn('If-number: ', CurrentToken.Lexeme);
     
     CurrentToken := GetNextToken;
     Match(_tkSemicolon);
-    writeln('----> ' + CurrentToken.Lexeme);
+    
+    if CurrentToken.TokenType = _tkIdentifier then
+    begin
+      WriteLn('ident: semico: ', CurrentToken.Lexeme);
+      ParseStatement;
+    end;
   end else
-  raise Exception.Create('Syntax Error: Expected identifier as condition');
-
-  Match(_tkThen);
-  ParseStatement;
-
-  if CurrentToken.TokenType = _tkElse then
-  begin
-    Match(_tkElse);
-    ParseStatement;
-  end;
+  yyerror('Expected identifier as condition');
 end;
 
 procedure ParseProgram;
@@ -261,8 +562,9 @@ begin
           begin
             CurrentToken := GetNextToken;
             if CurrentToken.TokenType = _tkIdentifier then ParseStatement;
+            if CurrentToken.TokenType = _tkIf         then ParseIfStatement;
             if CurrentToken.TokenType = _tkEnd        then break;
-            if CurrentToken.TokenType = _tkEOF        then raise Exception.Create('error: end of file.');
+            if CurrentToken.TokenType = _tkEOF        then yyerror('end of file');
           end;
         end;
         if CurrentToken.TokenType = _tkEnd then
@@ -288,19 +590,14 @@ begin
   Source       := ''
   + 'program test; '
   + 'begin '
-  + 'x := 42; '
+  + 'x := 42 + (11 + xxy + (12 + (9 * 2 + (2))) );'
   + 'if x then yx := 2 else ayc := 3; '
-  + 'y :=  7;'
+  + 'cy :=  7;'
   + 'end.'
   ;
   
-  try
-    InitScanner(Source);
-    ParseProgram;
-  except
-    on E: Exception do
-      WriteLn('Error: ', E.Message);
-  end;
+  InitScanner(Source);
+  ParseProgram;
   
   ExitProcess(0);
 end.
